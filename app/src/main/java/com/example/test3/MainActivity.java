9c14b5ae036003ab62949d21518da6a49523f2b9
Hasan El-Hefnawy <helhefnawy89@yahoo.com>
2020-02-22 21:06:22 +0200

    brightcove inside scrollview

--- app/src/main/java/com/example/test3/MainActivity.java
+++ app/src/main/java/com/example/test3/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.test3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.brightcove.player.appcompat.BrightcovePlayerActivity;
+import com.brightcove.player.model.DeliveryType;
+import com.brightcove.player.model.Video;
+import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
+import com.brightcove.player.view.BrightcovePlayer;
+
+import java.net.URISyntaxException;
+
+public class MainActivity extends BrightcovePlayer {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BrightcoveExoPlayerVideoView brightcoveVideoView = findViewById(R.id.brightcove_video_view);
+        Video video = Video.createVideo("https://learning-services-media.brightcove.com/videos/hls/greatblueheron/greatblueheron.m3u8", DeliveryType.HLS);
+        try {
+            java.net.URI myposterImage = new java.net.URI("https://solutions.brightcove.com/bcls/assets/images/Great-Blue-Heron.png");
+            video.getProperties().put(Video.Fields.STILL_IMAGE_URI, myposterImage);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        brightcoveVideoView.add(video);
+        brightcoveVideoView.start();
     }
 }
